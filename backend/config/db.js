30727eb696c52d988f8df42f500550886b4f16cc
dcm-mcd <doliecirim@gmail.com>
2026-07-13 17:53:16 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://anusridc67:Z9Fy85X0OydR7QvM@cluster0.u4xm90x.mongodb.net/food-del');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("DB Connected");
     } catch (error) {
         console.error("DB Connection Error:", error.message);
